e3aabc9650bf8dcbd5f2c9748406d341d690123d
sanday08 <sandip.job89@gmail.com>
2020-11-03 19:43:04 +0530

    add

--- start.js
+++ start.js
@@ -12,11 +12,26 @@ var con = mysql.createConnection({
   multipleStatements: true
 });
 let betTypes = { blurs: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], parity: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], sapre: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], bcon: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-let startGame = false;
+let startGame = true;
 let startTime = new Date().getTime();
 let randomWinner = { blurs: -1, parity: -1, sapre: -1, bcon: -1 };
 let userBet = { blurs: [], parity: [], sapre: [], bcon: [] };
 //userId,betAmount,betType
+let last10Bets = []
+
+function getLast10Bets() {
+  con.query("SELECT * FROM bet_history ORDER BY created DESC LIMIT 10", function (err, result) {
+    if (!err) {
+      console.log("sandip", startTime);
+      last10Bets = result;
+    }
+    else {
+      io.local.emit("error", { msg: "Right now server is not connected try again later!" })
+    }
+  });
+}
+getLast10Bets()
+
 io.on("connection", (socket) => {
   console.log("manu")
 
@@ -24,33 +39,34 @@ io.on("connection", (socket) => {
   socket.on("join", () => {
     console.log("sandip", startTime);
 
-    con.query("SELECT * FROM bet_history ORDER BY created DESC LIMIT 10", function (err, result) {
-      if (!err) {
-        console.log("sandip", startTime);
-        io.to(socket.id).emit("join", { betHistory: result, countDown: (new Date().getTime() - startTime) / 1000 })
-      }
-      else {
-        io.to(socket.id).emit("error", { msg: "Right now server is not connected try again later!" })
-      }
-    });
+    io.to(socket.id).emit("join", { betHistory: last10Bets, countDown: (new Date().getTime() - startTime) / 1000 })
 
   })
 
-  socket.join();
+  io.on("userCurrentBet", ({ userId }) => {
+    con.query("SELECT * FROM user_bet_history where user_id =? and status =0", [userId], function (err, result) {
+      if (err)
+        io.to(socket.id).emit("error", { msg: "Error for get data" })
+      else
+        io.to(socket.id).emit("join", { currentBet: result })
+    })
+  })
+
+
   socket.on("placeBet", ({ id, password, betType, betAmount, betCategory }) => {
     if (startGame) {
       con.query("select * from user where id =? and password =?", [id, password], function (err, result) {
         if (err) {
-          io.to(socket.id).emit("error",{ msg: "Error:" + err.message()});
+          io.to(socket.id).emit("error", { msg: "Error:" + err.message() });
         }
         else {
           if (result.amount < betAmount) {
-            io.to(socket.id).emit("error",{ msg: "InSufficient Balance Please Deposit Amount..!"});
+            io.to(socket.id).emit("error", { msg: "InSufficient Balance Please Deposit Amount..!" });
           }
           else {
             con.query("UPDATE customers SET amount =amount-?  where id =? and password =?", [betAmount, id, password], function (err, result) {
               if (err) {
-                io.to(socket.id).emit("error",{ msg:"Error" + err.message()});
+                io.to(socket.id).emit("error", { msg: "Error" + err.message() });
               }
               else {
                 con.query("SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'kraazy' AND TABLE_NAME = 'bet_history'", (err, result) => {
@@ -59,9 +75,9 @@ io.on("connection", (socket) => {
                     let winAmount = 0;
                     let commission = betAmount > 100 ? betAmount * 2 / 100 : 2
                     con.query("insert into user_bet_history (user_id,amount,select,result,fee,delivery,status,category,bet_history_id) values (?,?,?,?,?,?,?,?,?)",
-                      [id, betAmount, betType, -1, commission, 0, 2, betCategory, period], function (err, result) {
+                      [id, betAmount, betType, -1, commission, 0, 0, betCategory, period], function (err, result) {
                         if (err) {
-                          io.to(socket.id).emit("error",{ msg:"Error" + err.message()});
+                          io.to(socket.id).emit("error", { msg: "Error" + err.message() });
                         }
                         else {
                           if (betType === "green" || betType === "red" || betType === "blue") {
@@ -106,52 +122,62 @@ io.on("connection", (socket) => {
       );
     }
     else {
-      io.to(socket.id).emit("error",{ msg:"you can not place any bet"})
+      io.to(socket.id).emit("error", { msg: "you can not place any bet" })
     }
   });
   socket.on("disconnect", async () => {
   });
 
-  setInterval(() => {
-    if (startTime + (1000 * 180) < new Date().getTime()) {
-      //check the total
-      startTime = new Date().getTime();
-      for (let betCategory of Object.keys(userBet)) {
-        const result = getMinKeys(betTypes[betCategory]);
-        const random = Math.floor(Math.random() * result.length);
-        randomWinner[betCategory] = result[random];
-        //Give winner users that amount
-        if (userBet[betCategory]) {
-          for (let bet in userBet[betCategory]) {
-            if (bet.betType === finalAns) {
-              //con.query("UPDATE user SET address = 'Canyon 123' WHERE address = 'Valley 345'")
-            }
-            else if ((finalAns >= 0 && finalAns < 5) && bet.betType === "green") {
 
-            }
-            else if ((finalAns > 4 && finalAns < 10) && bet.betType === "red") {
+});
+setInterval(() => {
 
-            }
-            else if ((finalAns === 0 || finalAns === 5) && bet.betType === "blue") {
+  if (startTime + (1000 * 180) < new Date().getTime()) {
+    //check the total
+    startTime = new Date().getTime();
+    for (let betCategory of Object.keys(userBet)) {
+      const result = getMinKeys(betTypes[betCategory]);
+      const random = Math.floor(Math.random() * result.length);
+      randomWinner[betCategory] = result[random];
+      //Give winner users that amount
+      if (userBet[betCategory]) {
+        for (let bet in userBet[betCategory]) {
+          if (bet.betType === finalAns) {
+            //con.query("UPDATE user SET address = 'Canyon 123' WHERE address = 'Valley 345'")
+          }
+          else if ((finalAns >= 0 && finalAns < 5) && bet.betType === "green") {
 
-            }
+          }
+          else if ((finalAns > 4 && finalAns < 10) && bet.betType === "red") {
+
+          }
+          else if ((finalAns === 0 || finalAns === 5) && bet.betType === "blue") {
+
           }
         }
-
       }
-      con.query("insert into bet_history (blurs_no,blurs_price,parity_no,parity_price,sapre_no,sapre_price,bcon_no,bcon_price) values (?, ?, ?, ?, ?, ?, ?, ?)", [randomWinner.blurs, Math.floor(Math.random() * 100000) + 100000, randomWinner.parity, Math.floor(Math.random() * 10000) + 10000, randomWinner.sapre, Math.floor(Math.random() * 10000) + 10000, randomWinner.bcon, Math.floor(Math.random() * 10000) + 10000], function (err, result) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log(result);
-          io.to(socket.id).emit("result",result)
-        }
 
-      })
     }
-  }, 1000);
-});
+    con.query("insert into bet_history (blurs_no,blurs_price,parity_no,parity_price,sapre_no,sapre_price,bcon_no,bcon_price) values (?, ?, ?, ?, ?, ?, ?, ?)", [randomWinner.blurs, Math.floor(Math.random() * 100000) + 100000, randomWinner.parity, Math.floor(Math.random() * 10000) + 10000, randomWinner.sapre, Math.floor(Math.random() * 10000) + 10000, randomWinner.bcon, Math.floor(Math.random() * 10000) + 10000], function (err, result) {
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(result);
+        io.local.emit("result", "refresh");
+        startGame = true;
+      }
+      getLast10Bets()
+    })
+  }
+  else if (startTime + (1000 * 150) < new Date().getTime()) {
+    startTime = false;
+  }
+}, 1000);
+
+
+
 
 function getMinKeys(array) {
   var min = Math.min.apply(Math, array);
